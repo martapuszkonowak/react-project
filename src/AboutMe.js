46cea1bef775e0e22c38faac12658e8f30365d0b
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-10-11 18:42:15 +0100

    Added pictures,  created new component Projects

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,15 +1,15 @@
 import React from "react";
+import Projects from "./Projects";
 import "./AboutMe.css";
 
-export default function AboutMe() {
-  let name = "Marta Nowak";
-  let age = 40;
+export default function AboutMe(props) {
   return (
     <div className="AboutMe">
       <h3>
-        Hi, I'm {name}. I'm {age} years old and I like to code.
+        Hi, I'm {props.name}. I'm {props.age} years old and I like to code.
       </h3>
-      <p>Here I try to show off my programming skills..</p>
+      <p>See my latest projects:</p>
+      <Projects />
     </div>
   );
 }
