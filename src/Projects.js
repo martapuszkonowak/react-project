d44825e0e3b2f5bac0413b8113d1f2b8c2ded05b
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-10-13 20:01:37 +0100

    updated css

--- src/Projects.js
+++ src/Projects.js
@@ -5,20 +5,20 @@ export default function Projects() {
   return (
     <div className="Projects">
       <h4>Here they are!</h4>
-      <div className="Container">
+      <div className="container">
         <div className="row">
           <div className="col-6">
             <img
               src="/images/dictionary.png"
               alt="Dictionary project preview"
-              className="img-fluid rounded shadow"
+              className="img-fluid rounded box-shadow"
             />
           </div>
           <div className="col-6">
             <img
               src="/images/weather.png"
               alt="Weather project preview"
-              className="img-fluid rounded shadow"
+              className="img-fluid rounded box-shadow"
             />
           </div>
         </div>
